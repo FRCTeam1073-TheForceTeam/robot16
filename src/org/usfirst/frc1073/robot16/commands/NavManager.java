b770bc41a32d1d2e05fb52b2ef8e0763b2aa23e3
Matt Watrous <wmfish@watrousweb.com>
2016-02-06 11:44:28 -0500

    Unfinished Nav Integration, Map Methods Altered

--- src/org/usfirst/frc1073/robot16/commands/NavManager.java
+++ src/org/usfirst/frc1073/robot16/commands/NavManager.java
@@ -38,10 +38,13 @@ public class NavManager extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.navigation.initializeMap();
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc1073/robot16/navClasses/gameObjects/Map.java
+++ src/org/usfirst/frc1073/robot16/navClasses/gameObjects/Map.java
@@ -105,7 +105,9 @@ public class Map{
 	 */
 	
 	public void initializeMap(double robotX, double robotY){
+		RobotNav testRobot = new RobotNav(2.0,2.0,1.0,1.0,this);
 		
+		mapArray = testRobot.getMapWithRobotArray();
 	}
 
 
