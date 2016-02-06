95138ec6bd49579db60a8915a8539186f94a4e13
Kyle Pittman <kyle.pittman.first@gmail.com>
2016-02-06 10:49:38 -0500

    I'm not a loser :(

--- src/org/usfirst/frc1073/robot16/navClasses/gameObjects/RobotNav.java
+++ src/org/usfirst/frc1073/robot16/navClasses/gameObjects/RobotNav.java
@@ -8,17 +8,11 @@ import org.usfirst.frc1073.robot16.navClasses.Core.RegionManager;
  */
 
 public class RobotNav {
-	private int[][] robotArray; //Holds the space the robot occupies
 	private int[][] mapArray; //Holds the imported map array
 	
-	private double robotXWidth; //ft.decInch
-	private double robotYWidth; //ft.decInch
 	
 	public RobotNav(double xStart, double yStart, double robotXWidth, double robotYWidth, Map gameMap) {
-		this.robotXWidth = robotXWidth; //TODO May be unnecessary...
-		this.robotYWidth = robotYWidth; //TODO May be unnecessary...
-				
-		robotArray = RegionManager.rectangleBuilder(xStart, yStart, robotXWidth, robotYWidth, 1); //Return the array for the robot
+		
 		mapArray = gameMap.getMapArray(); //Return the array for the map
 		
 		for(int x = (int) (10 * xStart); x < (10 * (xStart + robotXWidth)); x++){
@@ -28,8 +22,7 @@ public class RobotNav {
 		}
 	}
 	
-	public int[][] getMapRobotArray(){
-		//return robotArray;
+	public int[][] getMapWithRobotArray(){
 		return mapArray;
 	}
 	
