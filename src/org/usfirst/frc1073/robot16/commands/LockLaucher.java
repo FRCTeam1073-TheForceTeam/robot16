8e669eaf1cf10665d4e9a16ab595b544880f0566
Keenan Troddyn <Keenan@Keenans-MacBook-Pro.local>
2016-02-22 17:03:38 -0500

    Stops motor after locking mechanism locks

--- src/org/usfirst/frc1073/robot16/commands/LockLaucher.java
+++ src/org/usfirst/frc1073/robot16/commands/LockLaucher.java
@@ -70,6 +70,7 @@ public class LockLaucher extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.laucher.stopLaucherMotor();
     }
 
     // Called when another command which requires one or more of the same
