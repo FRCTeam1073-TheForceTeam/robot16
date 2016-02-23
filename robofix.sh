c2f6c43c09c637569d55c687ff64837d6bf8e490
Keenan Troddyn <Keenan@Keenans-MacBook-Pro.local>
2016-02-22 19:40:35 -0500

    Added defense stuff ; Shell script for robo builder breaks

---
+++ robofix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git checkout -- src/org/usfirst/frc1073/robot16/subsystems/Laucher.java
+git checkout -- src/org/usfirst/frc1073/robot16/subsystems/DriveTrain.java
+git checkout -- src/org/usfirst/frc1073/robot16/commands/Drive.java 
