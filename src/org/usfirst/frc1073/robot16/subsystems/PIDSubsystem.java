e94cb5e51cc9d4ff5ce1245e08a479ad17c04c71
nnapior <nnapior@gmail.com>
2016-03-07 20:19:04 -0500

    New Robot code built off of a fresh robot builder

--- src/org/usfirst/frc1073/robot16/subsystems/PIDSubsystem.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc1073.robot16.subsystems;
-/**
-* An interface for use with PIDThread. Any subsystem that will be using a PIDThread must implement this interface
-*/
-public interface PIDSubsystem {
-	public double getPIDSource(int marker);
-	public void setPIDOutput(double output, int marker);
-}
